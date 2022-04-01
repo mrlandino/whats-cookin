bcd374baea6cde3f793dc7213cdd0569fca2afb5
Ross Landino <mrlandino@gmail.com>
2022-04-01 12:00:09 -0600

    Add files for User class and User test

---
+++ src/classes/User.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(user) {
+    this.name = user.name;
+    this.id = user.id;
+    this.pantry = user.pantry;
+    this.favoriteRecipes = [];
+  };
+
+  favoriteRecipes(recipe) => {
+    //this will run when the user favorites a recipe. it will then add to this.favoriteRecipes array
+
+    
+  }
+}
+
+export default User;
