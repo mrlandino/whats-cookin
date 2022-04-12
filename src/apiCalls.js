dce7e3f5fd4a88975ef9f22f482a86ed85a5e86a
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-04-12 15:55:41 -0600

    Dry up api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,11 @@
-  let usersPromise = fetch("http://localhost:3001/api/v1/users")
+let getPromise = (url) => {
+  return fetch(url)
   .then(usersResponse => usersResponse.json())
   .catch(err => console.log(error));
+};
 
-  let ingredientsPromise = fetch("http://localhost:3001/api/v1/ingredients")
-  .then(ingredientsResponse => ingredientsResponse.json())
-  .catch(err => console.log(error));
-
-  let recipePromise = fetch("http://localhost:3001/api/v1/recipes")
-  .then(recipeResponse => recipeResponse.json())
-  .catch(err => console.log(error));
-
-
-// Put urls into array?
-// forEach on them for the then and catch?
-
-// let urlsArray = []
+let usersPromise = getPromise("http://localhost:3001/api/v1/users");
+let ingredientsPromise = getPromise("http://localhost:3001/api/v1/ingredients");
+let recipePromise = getPromise("http://localhost:3001/api/v1/recipes");
 
 export {usersPromise, ingredientsPromise, recipePromise};
