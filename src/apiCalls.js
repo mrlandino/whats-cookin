730f8ca50ed72767821ffc1c642424a7cba04a06
Ross Landino <mrlandino@gmail.com>
2022-04-16 18:05:34 -0600

    Add POST request to add ingredient to pantry

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+let errorMessage = document.querySelector(".error-message");
+let userUrl = 'http://localhost:3001/api/v1/users';
 let getPromise = (url) => {
   return fetch(url)
   .then(response => response.json())
@@ -8,4 +10,31 @@ let usersPromise = getPromise("http://localhost:3001/api/v1/users");
 let ingredientsPromise = getPromise("http://localhost:3001/api/v1/ingredients");
 let recipePromise = getPromise("http://localhost:3001/api/v1/recipes");
 
-export {usersPromise, ingredientsPromise, recipePromise};
+let postIngredient = (postObject) => {
+  fetch(userUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(postObject)
+  })
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response.statusText);
+    }
+  })
+  .then((ingredient) => {
+    errorMessage.innerText = '';
+    console.log("Sweet Your Ingredient is Added")
+  })
+  .catch((error) => {
+    errorMessage.innerText = 'Your ingredient was not added successfully';
+    return errorMessage;// container to display errorMessage(querySelector)
+  });
+};
+
+const showAddition = (ingredient) => {
+  console.log(ingredient);
+
+}
+export {usersPromise, ingredientsPromise, recipePromise, postIngredient};
